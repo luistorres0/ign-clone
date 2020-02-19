80c3fe4192ad7f2536506f48415477c452f02ef8
Luis Torres <luisdtor@gmail.com>
2020-02-19 16:11:39 -0600

    added links to drop down menu

--- script.js
+++ script.js
@@ -14,3 +14,20 @@ let date = dt.getDate();
 document.getElementById("date-weekday").innerHTML = day + ',';
 document.getElementById("date-monthDate")
     .innerHTML = month + ' ' + date;
+
+// TOGGLE MENU
+let isDropDownOpen = false;
+const onToggleHandler = (event) => {
+    isDropDownOpen = !isDropDownOpen;
+
+    const menu = document.getElementById("toggle-menu");
+    if(isDropDownOpen) {
+        menu.style.height = "calc(100vh - 45px)";
+        event.target.className += " active";
+    } else {
+        menu.style.height = "0";
+        event.target.className = event.target.className.replace(" active", "");
+    }
+}
+document.getElementById("toggle-button").onclick
+     = onToggleHandler;
